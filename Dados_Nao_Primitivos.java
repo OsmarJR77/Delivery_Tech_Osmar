224945b63f7e2d6d46d5f6923376a18ac9e3b0ff
OsmarJR77 <osmar.oliveira2@fatec.sp.gov.br>
2025-10-08 22:22:47 -0300

    Adicionando novas linhas no exemplo

--- Dados_Nao_Primitivos.java
+++ Dados_Nao_Primitivos.java
@@ -6,7 +6,18 @@ public class Dados_Nao_Primitivos {
 
     String nomeMaisculo = nome.toUpperCase();   // a vaiavel nomeMaiusculo recebe o valor de nome em MAIUSCULO
 
-    System.out.println(" O nome " + nome + " agora texto está em MAIUSCULO :  " + nomeMaisculo);
+    System.out.println(" - - - - - - -  ");   // Pula uma linha em vazio
+    System.out.println("O nome " + nome + " agora texto está em MAIUSCULO :  " + nomeMaisculo);
+    String aldeia = "ALDEIA DA FOLHA";
+
+    String aldeiaMinusculo = aldeia.toLowerCase();  // a variavel aldeiaMinusculo recebe o valor de aldeia em minusculo
+
+    System.out.println(" - - - - - - - ");   // Pula uma linha em vazio
+    System.out.println(aldeia + " que é o nome da aldeia do " + nome + " está em minusculo :  " + aldeiaMinusculo);
+
+    System.out.println(" ");   // Pula uma linha em vazio
+
+
 
   }
 }
