5aab4c7006c07d5948753b4c65b3ac7e88504a8d
OsmarJR77 <osmar.oliveira2@fatec.sp.gov.br>
2025-10-08 21:45:33 -0300

    Adicionando um exemplo de variavel nao primitiva

---
+++ Dados_Nao_Primitivos.java
@@ -0,0 +1,12 @@
+public class Dados_Nao_Primitivos {
+
+  public static void main(String[] args) {
+    
+    String nome = "Naruto Uzumaki";     // String é um tipo não primitivo de dados
+
+    String nomeMaisculo = nome.toUpperCase();   // a vaiavel nomeMaiusculo recebe o valor de nome em MAIUSCULO
+
+    System.out.println(" O nome " + nome + " agora texto está em MAIUSCULO :  " + nomeMaisculo);
+
+  }
+}
